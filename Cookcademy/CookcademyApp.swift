509945880af7b581e70baf563d4857c91633efe8
vadimbeast <vadim.reklama58@yandex.ru>
2022-07-03 16:14:53 +0300

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Вадим Селиверстов on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
